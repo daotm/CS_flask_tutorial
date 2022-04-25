16c98fdb8078611002a67bb6abfa1f8cb69b57ca
Wind Dude <trietdao.supersmash@gmail.com>
2022-04-24 23:50:57 -0400

    registration form

--- flashblog.py
+++ flashblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '79a12b36c38394040b2af0a00197e585'
 
 
 posts = [
@@ -16,8 +20,7 @@ posts = [
         'date_post': 'April 25, 2018'
     },
 ]
-
-
+    
 
 
 
@@ -30,6 +33,17 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
