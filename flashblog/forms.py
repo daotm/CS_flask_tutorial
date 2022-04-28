02f3c86e8c8c18101565104b0691143092e12fe1
Wind Dude <trietdao.supersmash@gmail.com>
2022-04-27 23:44:56 -0400

    Day 6 half way

--- flashblog/forms.py
+++ flashblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flashblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -10,6 +10,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('User name taken')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email taken')
 
 
 class LoginForm(FlaskForm):
---
+++ flashblog/routes.py
@@ -0,0 +1,61 @@
+from flask import render_template, url_for, flash, redirect
+from flashblog import app, db, bcrypt
+from flashblog.forms import RegistrationForm, LoginForm
+from flashblog.models import User, Post
+from flask_login import login_user
+
+posts = [
+    {
+        'author': 'Triet Dao',
+        'title': 'Post 1',
+        'content': 'First post',
+        'date_post': 'April 22, 2022'
+    },
+    {
+        'author': 'Tom Schafer',
+        'title': 'Post 2',
+        'content': 'Second post',
+        'date_post': 'April 25, 2018'
+    },
+]
+    
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}! You are able to log in', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful', 'danger')
+            
+    return render_template('login.html', title='Login', form=form)
