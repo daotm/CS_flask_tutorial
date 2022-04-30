0e5789be5aeb58ab449879a46df2242f68b2115f
Wind Dude <trietdao.supersmash@gmail.com>
2022-04-30 19:55:39 -0400

    Add profile picture & capability to update account info

--- flashblog/forms.py
+++ flashblog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flashblog.models import User
+from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -27,3 +30,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me Label?')
     submit = SubmitField('Login')
+
+
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User name taken')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email taken')
