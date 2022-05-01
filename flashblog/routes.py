1029b5140d121193bc7b2aa9fac10256abe20625
Wind Dude <trietdao.supersmash@gmail.com>
2022-04-30 23:26:28 -0400

    Add delete route and buttons

--- flashblog/routes.py
+++ flashblog/routes.py
@@ -111,7 +111,8 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
-@app.route("/post/<int:post_id>/update",  methods=['GET','POST'])
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -124,7 +125,20 @@ def update_post(post_id):
         db.session.commit()
         flash('Your post has been updated!', 'success')
         return redirect(url_for('post', post_id=post.id))
-    form.title.data = post.title
-    form.content.data = post.content
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template('create_post.html', title='Update Post',
-                             form = form, legend='New Post')
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
